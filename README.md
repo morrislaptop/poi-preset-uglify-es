76b576d9e630235c2165b9f4d6b652b03721c52a
Craig Morris <cr@igmorr.is>
2018-02-07 16:54:41 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # poi-preset-uglify-es
-Use uglify-es instead of uglify-js
+
+Use [uglify-es](https://github.com/mishoo/UglifyJS2/tree/harmony) to minimize JavaScript.
+
+## Install
+
+```bash
+yarn add poi-preset-uglify-es --dev
+```
+
+## Usage
+
+This preset will replace UglifyjsPlugin with the latest [uglifyjs-webpack-plugin](https://github.com/webpack-contrib/uglifyjs-webpack-plugin) for you:
+
+```js
+// poi.config.js
+module.exports = {
+  presets: [
+    require('poi-preset-uglify-es')()
+  ]
+}
+```
+
+Then it will compress JS code when you run `poi build`.
+
+It accepts the same options as which in [uglifyjs-webpack-plugin](https://github.com/webpack-contrib/uglifyjs-webpack-plugin#options):
+
+```js
+require('poi-preset-uglify-es')({ parallel: true, uglifyOptions: { warnings: false }})
+```
+
+## License
+
+MIT &copy; [EGOIST](https://github.com/morrislaptop)
---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = (pluginOptions) => {
+  return poi => {
+    poi.extendWebpack('production', config => {
+      
+      pluginOptions = poi.merge({
+        sourceMap: true,
+        uglifyOptions: {
+          compressor: {
+            warnings: false,
+            conditionals: true,
+            unused: true,
+            comparisons: true,
+            sequences: true,
+            dead_code: true,
+            evaluate: true,
+            if_return: true,
+            join_vars: true,
+            negate_iife: false
+          },
+          output: {
+            comments: false
+          }
+        }
+      }, pluginOptions)
+
+      // Say goodbye to uglify plugin
+      const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+      // do not use if `minimize` is off
+      if (config.plugins.has('minimize')) {
+        config.plugin('minimize')
+          .UglifyJsPlugin(UglifyJsPlugin, [pluginOptions])
+      }
+    })
+  }
+}
